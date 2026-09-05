b18e44afb5ddf6e4a4d7e490e92a066e8774f0ad
Rohit <rohitthirumump@gmail.com>
2026-09-06 00:35:08 +0530

    Change: Changed DB from in-memory H2 to Postgres and running it from Docker. Added: Added Docker compose

--- src/main/java/com/tripplanner/citybreak/CitybreakApplication.java
+++ src/main/java/com/tripplanner/citybreak/CitybreakApplication.java
@@ -3,10 +3,13 @@ package com.tripplanner.citybreak;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class CitybreakApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(CitybreakApplication.class, args);
 	}
 
